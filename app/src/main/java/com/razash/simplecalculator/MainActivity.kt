b4639ea15cf6c017007838ceab610fb9c1c9236a
Raz <razash10@gmail.com>
2020-08-02 15:55:46 +0300

    Added basic functionality

--- app/src/main/java/com/razash/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/razash/simplecalculator/MainActivity.kt
@@ -3,15 +3,36 @@ package com.razash.simplecalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var lastNumeric = false
+    private var lastDot = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View) {
-        Toast.makeText(this, "Button works!", Toast.LENGTH_SHORT).show()
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+    }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+
+    fun onDecimalPoint(view: View) {
+        if(lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }

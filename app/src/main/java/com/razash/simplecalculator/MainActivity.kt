5fd437974eab1433fae594374edacd819045b417
Raz <razash10@gmail.com>
2020-08-02 17:05:47 +0300

    Added working operators

--- app/src/main/java/com/razash/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/razash/simplecalculator/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.ViewCompat
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,11 +17,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ViewCompat.setLayoutDirection(window.decorView, ViewCompat.LAYOUT_DIRECTION_LTR)
     }
 
     fun onDigit(view: View) {
         tvInput.append((view as Button).text)
         lastNumeric = true
+
     }
 
     fun onClear(view: View) {
@@ -35,4 +39,93 @@ class MainActivity : AppCompatActivity() {
             lastDot = true
         }
     }
+
+    fun onEqual(view: View) {
+        if(lastNumeric) {
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+            try {
+                if(tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if(tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var left = splitValue[0]
+                    var right = splitValue[1]
+
+                    if(!prefix.isEmpty()) {
+                        left = prefix + left
+                    }
+
+                    tvInput.text =
+                        removeZeroAfterDot((left.toDouble() - right.toDouble()).toString())
+
+                } else if(tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var left = splitValue[0]
+                    var right = splitValue[1]
+
+                    if(!prefix.isEmpty()) {
+                        left = prefix + left
+                    }
+
+                    tvInput.text =
+                        removeZeroAfterDot((left.toDouble() + right.toDouble()).toString())
+
+                } else if(tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var left = splitValue[0]
+                    var right = splitValue[1]
+
+                    if(!prefix.isEmpty()) {
+                        left = prefix + left
+                    }
+
+                    tvInput.text =
+                        removeZeroAfterDot((left.toDouble() / right.toDouble()).toString())
+
+                } else if(tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var left = splitValue[0]
+                    var right = splitValue[1]
+
+                    if(!prefix.isEmpty()) {
+                        left = prefix + left
+                    }
+
+                    tvInput.text =
+                        removeZeroAfterDot((left.toDouble() * right.toDouble()).toString())
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String) : String {
+        var value = result
+        if(result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+        return value
+    }
+
+    fun onOperator(view: View) {
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if(value.startsWith("-")) {
+            false
+        } else {
+            (value.contains("+") || value.contains("-") ||
+                    value.contains("*") || value.contains("/"))
+        }
+    }
+
 }
